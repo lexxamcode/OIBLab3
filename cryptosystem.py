d1438ffb134581275d7fc0cbd7a6e6f0cd6b0c2a
Alexey Mishkin <lexxam@vk.com>
2021-11-13 10:19:30 +0400

    Fixing all basic functions of cryptosystem.py, Realization of key generator, encrypting data and decrypting data

--- cryptosystem.py
+++ cryptosystem.py
@@ -1,7 +1,8 @@
 import os
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives import padding as pad
+from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
@@ -24,7 +25,7 @@ def deserialize_symmetric_key(path):
 
 
 def padding_text(text: str):
-    _padder = padding.ANSIX923(128).padder()
+    _padder = pad.ANSIX923(128).padder()
     _text = bytes(text, 'UTF-8')
 
     _padded_text = _padder.update(_text) + _padder.finalize()
@@ -32,23 +33,24 @@ def padding_text(text: str):
     return _padded_text
 
 
-def encrypt_text_with_symmetric_algorithm(key, padded_text):
+def encrypt_text_with_symmetric_algorithm(key, text: str):
     iv = os.urandom(16)
     cipher = Cipher(algorithms.Camellia(key), modes.CBC(iv))
-
     encryptor = cipher.encryptor()
-    e_text = encryptor.update(padded_text) + encryptor.finalize()
+    e_text = encryptor.update(padding_text(text)) + encryptor.finalize()
 
-    set_for_decryption = {'encrypted': e_text, 'iv': iv, 'cipher': cipher}
+    set_for_decryption = {'ciphrotext': e_text, 'iv': iv}
 
     return set_for_decryption
 
 
-def decrypt_text_symmetric_algorithm(key, encrypted_text, iv, cipher):
+def decrypt_text_symmetric_algorithm(encrypted_text, key, iv):
+    cipher = Cipher(algorithms.Camellia(key), modes.CBC(iv))
+
     decryptor = cipher.decryptor()
     decrypted_text = decryptor.update(encrypted_text) + decryptor.finalize()
 
-    unpadder = padding.ANSIX923(128).unpadder()
+    unpadder = pad.ANSIX923(128).unpadder()
     unpadded_decrypted_text = unpadder.update(decrypted_text) + unpadder.finalize()
 
     return unpadded_decrypted_text
@@ -56,29 +58,8 @@ def decrypt_text_symmetric_algorithm(key, encrypted_text, iv, cipher):
 
 def generate_asymmetric_keys():
     keys = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-    asymmetric_keys = {}
 
-    private_key = keys
-    public_key = keys.public_key()
-
-    asymmetric_keys['private'] = private_key
-    asymmetric_keys['public'] = public_key
-
-    return asymmetric_keys
-
-
-def serialize_public_key(public_key, path='public_key.pem'):
-    with open(path, 'wb') as public_file:
-        public_file.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
-                                                  format=serialization.PrivateFormat.TraditionalOpenSSL,
-                                                  encryption_algorithm=serialization.NoEncryption()))
-
-
-def serialize_private_key(private_key, path='private_key.pem'):
-    with open(path, 'wb') as private_file:
-        private_file.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
-                                                     format=serialization.PrivateFormat.TraditionalOpenSSL,
-                                                     encryption_algorithm=serialization.NoEncryption()))
+    return keys
 
 
 def deserialize_public_key(path='public_key.pem'):
@@ -98,14 +79,12 @@ def deserialize_private_key(path='private_key.pem'):
 
 
 def encrypt_symmetric_key(key, public_key):
-    _key = bytes(key, 'UTF-8')
-    encrypted_key = public_key.encrypt(_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                                                          algorithm=hashes.SHA256, label=None))
+    encrypted_key = public_key.encrypt(key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                                                             algorithm=hashes.SHA256(), label=None))
     return encrypted_key
 
 
 def decrypt_symmetric_key(encrypted_key, private_key):
     decrypted_key = private_key.decrypt(encrypted_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                                                                    algorithm=hashes.SHA256, label=None))
-
+                                                                        algorithm=hashes.SHA256(), label=None))
     return decrypted_key
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pickle
 import cryptosystem
 
 settings = {
@@ -10,19 +11,71 @@ settings = {
 }
 
 
+def keys_generator(block_size=128, path_public: str = 'public.pem', path_private: str = 'private.pem',
+                   path_symm: str = 'symmetric_key.txt'):
+    # Generating asymmetric keys:
+    keys = cryptosystem.generate_asymmetric_keys()
+    public_key = keys.public_key()
+    private_key = keys
+    # Generating and encrypting symmetric key:
+    symmetric_key = cryptosystem.encrypt_symmetric_key(cryptosystem.generate_symmetric_key(block_size//8), public_key)
+
+    # Serializing keys:
+    cryptosystem.serialize_symmetric_key(symmetric_key, path_symm)
+
+    with open(path_public, 'wb') as public_file:
+        public_file.write(public_key.public_bytes(encoding=cryptosystem.serialization.Encoding.PEM,
+                                                  format=cryptosystem.serialization.PublicFormat.SubjectPublicKeyInfo))
+
+    with open(path_private, 'wb') as private_file:
+        private_file.write(private_key.private_bytes(encoding=cryptosystem.serialization.Encoding.PEM,
+                                                     format=cryptosystem.serialization.PrivateFormat.TraditionalOpenSSL,
+                                                     encryption_algorithm=cryptosystem.serialization.NoEncryption()))
+
+
+def encrypt_data(path_initial: str = 'initial_file.txt',
+                 path_encrypted_text: str = 'encrypted.txt',
+                 path_private: str = 'private.pem',
+                 path_symm: str = 'symmetric_key.txt'):
+    # Reading data from files:
+    with open(path_symm, 'rb') as sym_file:
+        symmetric_key = sym_file.read()
+    with open(path_initial, 'rb') as text_file:
+        initial_text = text_file.read()
+    # Getting private key from file:
+    private_key = cryptosystem.deserialize_private_key(path_private)
+
+    # Decrypting symmetric key:
+    symmetric_key = cryptosystem.decrypt_symmetric_key(symmetric_key, private_key)
+    print(len(symmetric_key))
+    # Encrypting text:
+    encrypted_text = cryptosystem.encrypt_text_with_symmetric_algorithm(symmetric_key, initial_text.decode('UTF-8'))
+    # Saving encrypted text and initial vector to file:
+    with open('encrypted.txt', 'wb') as enc_file:
+        pickle.dump(encrypted_text, enc_file)
+
+
+def decrypt_data(path_encrypted_text: str = 'encrypted.txt', path_private: str = 'private.pem',
+                 path_encrypted_key: str = 'symmetric_key.txt', path_decrypted: str = 'decrypted.txt'):
+    # Getting symmetric key and private key:
+    with open(path_encrypted_key, 'rb') as sym_key_file:
+        symmetric_key = sym_key_file.read()
+    with open(path_private, 'rb') as pem_in:
+        private_bytes = pem_in.read()
+    private_key = cryptosystem.load_pem_private_key(private_bytes, password=None,)
+
+    # Decrypting encoded key
+    symmetric_key = cryptosystem.decrypt_symmetric_key(symmetric_key, private_key)
+
+    with open('encrypted.txt', 'rb') as encrypt_file:
+        encoded_text = pickle.load(encrypt_file)
+
+    decrypted_text = cryptosystem.decrypt_text_symmetric_algorithm(encoded_text['ciphrotext'], symmetric_key, encoded_text['iv'])
+    with open(path_decrypted, 'w') as dec:
+        dec.write(decrypted_text.decode('UTF-8'))
+
+
 if __name__ == '__main__':
-    s_key = cryptosystem.generate_symmetric_key(16)
-    cryptosystem.serialize_symmetric_key(s_key, settings['symmetric_key'])
-
-    with open(settings['initial_file'], 'r') as file:
-        text = file.read()
-
-    padded_text = cryptosystem.padding_text(text)
-    encrypted_text_set = cryptosystem.encrypt_text_with_symmetric_algorithm(s_key, padded_text)
-    decrypted_text = cryptosystem.decrypt_text_symmetric_algorithm(s_key, encrypted_text_set['encrypted'],
-                                                                   encrypted_text_set['iv'],
-                                                                   encrypted_text_set['cipher'],)
-    # print(text)
-    # print(padded_text)
-    print(encrypted_text_set['encrypted'])
-    print(decrypted_text.decode('utf-8'))
+    keys_generator(128)
+    encrypt_data()
+    decrypt_data()
